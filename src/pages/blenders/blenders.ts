2e6cafc4354d68bcd07de2a092c28227cf83db63
Hardik <hardik.pithva@lntinfotech.com>
2018-11-16 13:28:05 +0530

    New changes

--- src/pages/blenders/blenders.ts
+++ src/pages/blenders/blenders.ts
@@ -25,14 +25,20 @@ export class BlendersPage {
 
       loader.present();
 
-    this.recipeProvider.getBlender().then(result => {
-      console.log("Aditya:  Blenders Data: " + JSON.parse(result.data).blenders);
-      this.blenders = JSON.parse(result.data).blenders;
+    // this.recipeProvider.getBlender().then(result => {
+    //   console.log("Aditya:  Blenders Data: " + JSON.parse(result.data).blenders);
+    //   this.blenders = JSON.parse(result.data).blenders;
+
+    //   this.filteredBlenders = this.blenders;
+
+    //   loader.dismiss();
+    // });
+
+    this.blenders = JSON.parse('[{"name":"E310","image_url":"https://www.vitamix.com/media/other/images/E310-Black-Rglam-on-Gray-620-x-620.jpg","category":"Classic Blenders","status":"Working","desc":"Classic 48-ounce Container#Variable Speed Control#Pulse Control","price":"$299.95"},{"name":"5200 Standard","image_url":"https://www.vitamix.com/media/catalog/product/5200compact/images/CS-rglam-black.jpg","category":"Classic Blenders","status":"Working","desc":"Classic 64-ounce Container#Variable Speed Control#High/Low Controls#Available in Certified Reconditioned","price":"$449.95"},{"name":"Professional Series 750","image_url":"https://www.vitamix.com/media/other/images/GS-Heritage-Rglam-Copper.jpg","category":"Classic Blenders","status":"Working","desc":"64 oz. Low Profile Container#Variable Speed Control#Pulse Control#5 Pre-programmed settings#Available in Certified Reconditioned","price":"$599.95"},{"name":"780","image_url":"https://www.vitamix.com/media/catalog/product/780/images/GS-Touch-Programs-Black-Rglam.jpg","category":"Classic Blenders","status":"Working","desc":"64 oz. Low Profile Container#Variable Speed Control#Pulse Control#5 Pre-programmed settings#Touch screen controls","price":"$719.95"},{"name":"E310","image_url":"https://www.vitamix.com/media/other/images/E310-Black-Rglam-on-Gray-620-x-620.jpg","category":"Classic Blenders","status":"Working","desc":"Classic 48-ounce Container#Variable Speed Control#Pulse Control","price":"$299.95"}]');
 
       this.filteredBlenders = this.blenders;
 
       loader.dismiss();
-    });
 
   }
 
